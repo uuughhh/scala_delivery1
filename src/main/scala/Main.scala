c8ce3692c71cfd086b91a11b0f9e57ad93309429
ferejbo <felix.rej.bottolfsen@gmail.com>
2022-11-06 22:08:39 +0100

    Complete task 2a

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
 object Main extends App {
 
-  println("Hello World")
-  
+  // Task 2a
+  def initializeThread(callback: () => Unit): Thread = {
+    return new Thread(new Runnable {
+      def run() = {
+        callback()
+      }
+    })
+  }
+  val thread = initializeThread(() => { println("Hello World") })
+  // thread.run()
 }
