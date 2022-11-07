a50225a424c86a21c11f6b9a3a8652f0e00cf774
yyyvonne <47291085+uuughhh@users.noreply.github.com>
2022-11-07 15:56:31 +0100

    task2 b,c

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,32 @@
 object Main extends App {
 
-  println("Hello World")
+  new task2BC
   
+}
+
+// Task2 (b)(c)
+class task2BC extends Thread {
+
+  private var counter: Int = 0
+
+  def increaseCounter(): Unit = this.synchronized {
+    counter += 1
+  }
+
+  def printCounter():Unit = {
+    println(counter)
+  }
+
+  // function for threading
+  def thread(body: => Unit): Thread = {
+    val t = new Thread {
+      override def run() = body
+    }
+    t.start
+    t
+  }
+  // 3 threads 
+  val t1 = thread{increaseCounter()}
+  val t2 = thread{increaseCounter()}
+  val t3 = thread{printCounter()}
 }
